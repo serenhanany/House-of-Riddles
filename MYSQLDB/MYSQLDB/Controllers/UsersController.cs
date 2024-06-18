80104e773d64e437a6656bd6c362829b314b22da
serenhanany <101177220+serenhanany@users.noreply.github.com>
2024-06-18 10:42:27 +0300

    D

---
+++ MYSQLDB/MYSQLDB/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MYSQLDB;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly UserContext _context;
+
+    public UsersController(UserContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] UserModel user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return Ok(new { message = "User registered successfully!" });
+    }
+}
---
+++ MYSQLDB/MYSQLDB/UserContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MYSQLDB;
+
+public class UserContext : DbContext
+{
+    public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+    public DbSet<UserModel> Users { get; set; }
+}
+
+
